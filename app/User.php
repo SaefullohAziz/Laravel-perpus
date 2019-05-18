b21298d1b4165cd750ef7bec6421a0b2831af3b7
Saefulloh Aziz <16171234@smkifsmd.sch.id>
2019-05-18 11:31:38 +0700

    final

--- app/User.php
+++ app/User.php
@@ -47,6 +47,9 @@ class User extends Authenticatable
         return null !== $this->roles()->whereIn('name', $roles)->first();
     }
 
+
+
+
     public function hasAnyRole($role)
     {
         return null !== $this->roles()->where('name', $role)->first();
